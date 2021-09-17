2fe38a3558eb1a65084001f127fda4ef5a6fa048
xiaxin <1347486073@qq.com>
2021-09-17 18:29:26 +0800

    first

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC, useEffect, useState } from 'react';
+import styles from './index.less';
+
+
+const IndexPage:FC = ()=>{
+  return (
+    <div>
+      <h1 className={styles.title}>home</h1>
+    </div>
+  )
+}
+
+export default IndexPage;
